b3e1534723823995a6498fde9d4e8d8317e2ed5b
shivam9pal <shivampaul9794@gmail.com>
2025-09-04 23:24:49 +0530

    Done more work

--- Frontend/src/api/axiosConfig.js
+++ Frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api",
+  baseURL: "https://notesapp-production-1c3e.up.railway.app/api",
 });
 
 api.interceptors.request.use((config) => {
