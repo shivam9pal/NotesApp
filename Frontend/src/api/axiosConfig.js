6a01b9c310eebb36b6cdf4cbf10e21e0e3ce2e57
shivam9pal <shivampaul9794@gmail.com>
2025-09-05 00:30:01 +0530

    Done more work

--- Frontend/src/api/axiosConfig.js
+++ Frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://notesapp-production-1c3e.up.railway.app/api",
+  baseURL: "https://notesapp-production-1c3e.up.railway.app/api/auth/",
 });
 
 api.interceptors.request.use((config) => {
