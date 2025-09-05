ee6d6d945c00fdf5bbc82e28588a9f83d127c292
Shivam Pal <shivampaul9794@gmail.com>
2025-09-05 21:53:59 +0530

    Update axiosConfig.js
    Ec2 Api

--- Frontend/src/api/axiosConfig.js
+++ Frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://notesapp-production-9060.up.railway.app/api",
+  baseURL: "http://13.234.123.100:8080/api/",
 });
 
 api.interceptors.request.use((config) => {
